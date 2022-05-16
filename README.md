513dd55c23a52019c0f2c7d613a2c74bb169bbac
Andy Wegner <andy@andrewwegner.com>
2022-05-15 22:55:44 -0500

    Adding template, single cert path, adjusting root

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 - https://devcenter.heroku.com/articles/heroku-cli#standalone-installation-with-a-tarball
 - https://sanic.dev/en/guide/getting-started.html#install
 - https://medium.com/tech-insights/how-to-deploy-a-python-script-or-bot-to-heroku-in-5-minutes-a82de2d3ed40
+- https://sanic.dev/en/guide/deployment/running.html#sanic-server
+- http://blog.perwagnernielsen.dk/sanic_web_tutorial_part_2_templates.html
+
+pip freeze > requirements.txt
--- server.py
+++ server.py
@@ -1,13 +1,50 @@
 from sanic import Sanic
 from sanic.response import text
+from sanic import response
+from sanic_jinja2 import SanicJinja2
 import os
 
-app = Sanic("MyHelloWorldApp")
+app = Sanic("WovenCertVerificationApp")
+jinja = SanicJinja2(app, pkg_name="main")
+
+VALID_IDS = {
+    "asdfghjkl54321": {
+        "name": "Martin McFly",
+        "certificate_name": "Highly Recommended",
+        "issue_date": "May 2022",
+        "expire_date": "May 2023",
+        "expired": False,
+    },
+    "9876789AbCd": {
+        "name": "Martin McFly",
+        "certificate_name": "Recommended",
+        "issue_date": "Jan 2020",
+        "expire_date": "Jan 2021",
+        "expired": True,
+    },
+    "TOTALLYREAL": {
+        "name": "Martin McFly",
+        "certificate_name": "Highly Recommended",
+        "issue_date": "May 2020",
+        "expire_date": None,
+        "expired": False,
+    },
+}
 
 
 @app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+async def root_path(request):
+    return text("Ok!")
+
+
+@app.get("/certificate/validate/<cert_id:str>")
+async def hello_world(request, cert_id: str):
+    try:
+        certificate = VALID_IDS[cert_id]
+    except KeyError:
+        certificate = None
+
+    return jinja.render("index.html", request, context=certificate)
 
 
 app.run(host="0.0.0.0", port=os.environ["PORT"], debug=True, access_log=False)
