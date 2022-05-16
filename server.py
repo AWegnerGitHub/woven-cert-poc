bc57e37fb158cb97a5c4b25cef3a84cdfbefdeb8
Andy Wegner <andy@andrewwegner.com>
2022-05-15 20:16:18 -0500

    Add run command; Black

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 from sanic.response import text
+import os
 
 app = Sanic("MyHelloWorldApp")
 
@@ -7,3 +8,6 @@ app = Sanic("MyHelloWorldApp")
 @app.get("/")
 async def hello_world(request):
     return text("Hello, world.")
+
+
+app.run(host="0.0.0.0", port=os.environ["PORT"], debug=True, access_log=False, worker=1)
